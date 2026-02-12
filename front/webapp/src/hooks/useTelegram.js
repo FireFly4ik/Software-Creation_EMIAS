73e44402042022061c263d210d93f26e11b84a5c
Змеев Илья <zmeev.i.v@edu.mirea.ru>
2026-02-12 20:39:37 +0300

    add use telegram hook

---
+++ front/webapp/src/hooks/useTelegram.js
@@ -0,0 +1,12 @@
+const tg = window.Telegram.WebApp;
+
+export function useTelegram() {
+  const onClose = () => tg.close()
+
+  return {
+    tg,
+    onClose,
+    username: tg.initDataUnsafe?.user?.username || 'User',
+    photo: tg.initDataUnsafe?.user?.photo_url || null
+  }
+};
